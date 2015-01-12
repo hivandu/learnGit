5b717780cee434b54298ac4d5fecfdd3da0a09ff
Hivan Du <doo@hivan.me>
2015-01-12 14:11:54 +0800

    del some text

--- test.rb
+++ test.rb
@@ -42,15 +42,7 @@ class WordIndex
   def initialize
     @index = {}
   end
-  def add_to_index(obj, *phrases)
-    phrases.each do |phrase|
-      phrase.scan(/w[-\w']+/) do |word|
-        word.downcase!
-        @index[word] = [] if @index[word].nil?
-        @index[word].push(obj)
-      end
-    end
-  end
+
   def lookup(word)
     @index[word.downcase]
   end
